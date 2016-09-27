03f2fb761e6bffd9cdb49c626acea969ee798b94
Sam Mortimer <sam@mortimer.me.uk>
2016-09-26 21:41:01 -0700

    Enable sdcardsfs
    Kernel support is enabled so use it
    
    Change-Id: I51ddcfa4fcb17a51f1ebf66b5dd2d92b77832e97

--- device.mk
+++ device.mk
@@ -473,6 +473,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1﻿
 
+# Enable sdcardfs
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
 $(call inherit-product-if-exists, hardware/qcom/msm8994/msm8994.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8994/msm8994-gpu-vendor.mk)
 
